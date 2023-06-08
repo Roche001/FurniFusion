3b181226ec8ec2f528487f91cdd35d049da5492e
Roche Harold <rocheharold13@gmail.com>
2023-06-08 22:48:14 +0300

    update changes

---
+++ src/components/buy/Buy.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Buy.css";
+
+const Buy = () => {
+  return <div>Buy</div>;
+};
+
+export default Buy;
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Search";
+
+const Search = () => {
+  return <div>Search</div>;
+};
+
+export default Search;
