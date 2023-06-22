64efdfea9009c43174cc01b1c565195f3f62dff2
Roche Harold <rocheharold13@gmail.com>
2023-06-22 14:07:50 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,8 +4,14 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="home-banner"></div>
-      <div className="home-cat"></div>
+      <div className="home-banner">
+        <div className="banner-one">
+          <img src="./assets/Armchair.png" alt="armchair" loading="lazy" />
+        </div>
+        <div className="banner-two">
+          <h3>hello</h3>
+        </div>
+      </div>
     </div>
   );
 };
