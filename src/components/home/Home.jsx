6073809637e32755194c30f538f13c06ee9b09a1
Roche Harold <rocheharold13@gmail.com>
2023-06-22 10:25:19 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,11 +4,7 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="home-banner">
-        {" "}
-        <h2>Royal Living</h2>
-        <p>Designing Spaces, Inspiring Lives.</p>
-      </div>
+      <div className="home-banner"></div>
       <div className="home-cat"></div>
     </div>
   );
