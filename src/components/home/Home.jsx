564c6f526828183590cb0bad09d78d48d48116d7
Roche Harold <rocheharold13@gmail.com>
2023-07-09 08:42:58 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Home.css";
+import { BsSearch } from "react-icons/bs";
 
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="home-banner"></div>
+      <div className="home-banner">
+        <h2>Welcome to Sycamore Designs</h2>
+        <p>
+          Unlock the potential of your space with our innovative furniture
+          designs.
+        </p>
+        <div className="home-input">
+          <input type="text" placeholder="Search for a product" />
+          <div className="home-search">
+            {" "}
+            <h4>
+              <BsSearch />
+            </h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
