4897c577f0a98d612f639b84fe38789cf7172f51
Roche Harold <rocheharold13@gmail.com>
2023-08-16 19:59:13 +0300

    update changes

--- src/components/shipping/Shipping.jsx
+++ src/components/shipping/Shipping.jsx
@@ -5,6 +5,9 @@ const Shipping = () => {
   return (
     <div className="ship-container">
       <h3>Shipping & Return Policy</h3>
+      <div className="shipping">
+        <h5>Shipping Information:</h5>
+      </div>
     </div>
   );
 };
